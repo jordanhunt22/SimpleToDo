80e081dd2880d1735ec44c3ba063f4c8403662ef
Jordan Hunt <jordanhunt@fb.com>
2020-06-23 17:19:34 -0700

    Add field made functional

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List <String> items;
+
+    Button btnAdd;
+    EditText edItem;
+    RecyclerView rvItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        edItem = findViewById(R.id.edItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = edItem.getText().toString();
+                // add item to the model
+                items.add(todoItem);
+                // notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                edItem.setText("");
+
+            }
+        });
     }
 }
